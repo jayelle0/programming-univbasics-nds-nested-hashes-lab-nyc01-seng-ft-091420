79dcf878f2c4d885af4bcfab88029571fd48c6e9
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 01:05:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,11 +1,10 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-team = [
-{name:  "Don Gately", occupation: "Live-in Staff" },
- {name:  "Joelle van Dyne", occupation: "Radio Personality"},
-  { name:  "Pat Monteseian", occupation: "Staff" },
- { name:  "Kate Gompert", occupation: "None" },
- { name:  "Bruce Green", occupation: "Fan of Mildred" },
-]
+team = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
+       {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+       {:name=>"Kate Gompert", :occupation=>"None"},
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+       
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
