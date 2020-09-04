7433751e1d3acb6867044ce820de4e45f9c8867d
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 01:01:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,8 +1,8 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 team = [
-DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+{ name:  "Don Gately", occupation: "Live-in Staff" },
+ { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
  { name:  "Kate Gompert", occupation: "None" },
  { name:  "Bruce Green", occupation: "Fan of Mildred" },
 ]
