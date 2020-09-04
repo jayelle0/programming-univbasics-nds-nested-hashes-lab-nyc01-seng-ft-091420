8b5a61b73929a87940112bafe5060e6e403315de
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 01:20:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,6 @@
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
        ]
- =
        
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
