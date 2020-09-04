28adf86a04c3c5cd7f12e568efd744e3eef37cfb
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 01:07:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,9 +1,11 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
-result = [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
+result = [
+      {:name=>"Don Gately", :occupation=>"Live-in Staff"},
        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
        {:name=>"Pat Monteseian", :occupation=>"Staff"},
        {:name=>"Kate Gompert", :occupation=>"None"},
-       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+       {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+       ]
        
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
